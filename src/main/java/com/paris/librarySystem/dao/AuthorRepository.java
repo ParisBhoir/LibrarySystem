60675e230a72751af48d8db8b11d608bb79fbca9
Paris <parisbhoir@gmail.com>
2024-09-11 18:14:58 +0530

    searching author by name for existing author verification

--- src/main/java/com/paris/librarySystem/dao/AuthorRepository.java
+++ src/main/java/com/paris/librarySystem/dao/AuthorRepository.java
@@ -3,5 +3,8 @@ package com.paris.librarySystem.dao;
 import com.paris.librarySystem.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByName(String name);
 }
