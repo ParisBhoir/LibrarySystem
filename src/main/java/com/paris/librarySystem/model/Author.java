daa83b6ea6ea5f6901b90509e302d299c7897a06
Paris <parisbhoir@gmail.com>
2024-09-11 14:50:35 +0530

    Author model

---
+++ src/main/java/com/paris/librarySystem/model/Author.java
@@ -0,0 +1,21 @@
+package com.paris.librarySystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "author")
+    private Set<Book> books;
+
+}
