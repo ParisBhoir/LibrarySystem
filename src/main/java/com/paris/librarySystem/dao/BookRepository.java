693ea87c7409b5e81f1251dddc88ce8d3a4c31c5
Paris <parisbhoir@gmail.com>
2024-09-11 18:16:12 +0530

    searching Books by author name using native query and also searching using title

--- src/main/java/com/paris/librarySystem/dao/BookRepository.java
+++ src/main/java/com/paris/librarySystem/dao/BookRepository.java
@@ -2,6 +2,14 @@ package com.paris.librarySystem.dao;
 
 import com.paris.librarySystem.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query(value = "SELECT * FROM book b WHERE b.author_id=(select author_id from author where name=:name)",nativeQuery = true)
+    List<Book> findAllByAuthorName(String name);
+
+
+    Book findByTitle(String title);
 }
