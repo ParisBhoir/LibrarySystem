e1bd28cb7f6a66848bf042264a86045adfdc0bc9
Paris <parisbhoir@gmail.com>
2024-09-12 15:22:22 +0530

    Created method to find user by id

--- src/main/java/com/paris/librarySystem/dao/BorrowRecordRepository.java
+++ src/main/java/com/paris/librarySystem/dao/BorrowRecordRepository.java
@@ -3,5 +3,8 @@ package com.paris.librarySystem.dao;
 import com.paris.librarySystem.model.BorrowRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Long> {
+    List<BorrowRecord> findByUserId(Long userId);
 }
