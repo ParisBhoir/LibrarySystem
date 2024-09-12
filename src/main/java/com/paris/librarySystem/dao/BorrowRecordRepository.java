709d410a73b6b578baad8d49ac14b9f25337e613
Paris <parisbhoir@gmail.com>
2024-09-12 16:40:02 +0530

    searching BorrowRecords by userID and bookID using native query

--- src/main/java/com/paris/librarySystem/dao/BorrowRecordRepository.java
+++ src/main/java/com/paris/librarySystem/dao/BorrowRecordRepository.java
@@ -2,9 +2,14 @@ package com.paris.librarySystem.dao;
 
 import com.paris.librarySystem.model.BorrowRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Long> {
     List<BorrowRecord> findByUserId(Long userId);
+
+    @Query(value = "SELECT * FROM borrow_record b WHERE b.user_id=:userId AND b.book_id=:bookId",nativeQuery = true)
+    Optional<BorrowRecord> findByUserIdAndBookId(Long userId, Long bookId);
 }
